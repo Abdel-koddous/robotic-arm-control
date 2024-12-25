a526dfc37d26b20748afcda23c64ae0be39d4893
Abdelkoddous <abdelkoddouskhamsi1@gmail.com>
2024-12-25 15:39:38 +0100

    Added increasing range of motion of the elbow joint

--- nema-17-stepper-test/nema-17-stepper-test.ino
+++ nema-17-stepper-test/nema-17-stepper-test.ino
@@ -5,9 +5,12 @@ const int stepPin = 2;     // Connects to STEP on the driver
 const int dirPin = 3;      // Connects to DIR on the driver
 const int enablePin = 5;   // Optional: Connects to ENABLE on the driver
 const int homeSwitchPin = 7;
-int steps_command = 1000;
 // Create an AccelStepper object in DRIVER mode
 AccelStepper stepper(AccelStepper::DRIVER, stepPin, dirPin);
+
+int min_position = 500;
+int max_position = 1500;
+int step_increments = 250;
 
 void setup() {
   Serial.begin(9600);
@@ -19,12 +22,9 @@ void setup() {
   digitalWrite(enablePin, LOW);    // LOW to enable, HIGH to disable
 
   // Set the initial target position
-  Serial.println("Target Number Of steps = " + String(steps_command));
-  stepper.moveTo(steps_command);
+  Serial.println("Target Number Of steps = " + String(max_position));
+  stepper.moveTo(max_position);
 }
-
-int min_position = 0;
-int max_position = 1000;
 
 void loop() {
 
@@ -32,16 +32,20 @@ void loop() {
   
   if (stepper.currentPosition()==max_position)
   {
-    delay(1000);
+    //delay(250);
+    min_position = min_position - step_increments;
     Serial.println("Moving to new target " + String(min_position) + " steps");
     stepper.moveTo(min_position);
+    ;
 
   }
   else if (stepper.currentPosition()==min_position)
   {
-    delay(1000);
+    //delay(250);
+    max_position = max_position + step_increments;
     Serial.println("Moving to new target " + String(max_position) + " steps");
     stepper.moveTo(max_position);
+    
   }
   
 }
