6bdc8109815669fd4ccbe37b3ea063bb640a4b8b
Abdelkoddous <abdelkoddouskhamsi1@gmail.com>
2025-01-25 12:03:34 +0100

    Added support for steppers stop command

---
+++ simultanous-steppers-control/README.md
@@ -0,0 +1,22 @@
+## Simultaneous Steppers Control
+
+### Simultaneous Move Command
+
+To use the multiple move commands on the serial link (9600 baud rate), send a command string starting with 'm' followed by the motor commands. Each motor command consists of a motor ID, a direction (not yet supported), and the number of steps. 
+
+For example, the command `m002000m101000m201500` will:
+- Move motor 0 (1st joint) clockwise for 2000 steps,
+- Move motor 1 (2nd joint) clockwise for 1000 steps,
+
+  
+Make sure to monitor the serial output for feedback on the commands being processed and the positions of the motors.
+
+__Note 1__: For now only 2 motors are supported.
+
+__Note 2__: you can also use the `m` command to move a single motor.
+
+* Example: `m002000` will move motor 0 (1st joint) clockwise for 2000 steps.
+
+### Stop Command
+To stop all motors, send the command `s`. This will immediately stop all motor movements regardless of their current position.
+
--- simultanous-steppers-control/simultanous-steppers-control.ino
+++ simultanous-steppers-control/simultanous-steppers-control.ino
@@ -5,33 +5,32 @@
 AccelStepper stepper1(motorInterfaceType, 3, 2); // Base motor (1st joint)
 AccelStepper stepper2(motorInterfaceType, 5, 4); // Shoulder motor (2nd joint)
 
-void setup() {
-  Serial.begin(9600); // Start serial communication
-  stepper1.setMaxSpeed(500); // Set maximum speed for stepper 1
-  stepper1.setAcceleration(1000); // Set acceleration for stepper 1
-  stepper2.setMaxSpeed(500); // Set maximum speed for stepper 2
-  stepper2.setAcceleration(1000); // Set acceleration for stepper 2
-}
 
-void loop() {
-  if (Serial.available() > 0) {
-    String command = Serial.readStringUntil('\n'); // Read command until newline
-    
-    //processCommand(command); // Process the command
-    parseMultipleMoveCommands(command);
-  }
-  else
+void parseInputCommand(String command)
+{
+  /*
+  * This function is used to parse the input command and execute the appropriate actions for the stepper motors. 
+  * Current supported commands:
+  * - 's': Stop command. Stops all motors immediately.
+  * - 'm': Move command. Initiates movement for specified motors based on the sub-commands.
+  */
+  if (command.startsWith("s"))
   {
-    stepper1.run(); // Run the stepper 1
-    stepper2.run(); // Run the stepper 2
+    Serial.println("Stop command received - Stopping all motors");
+    stepper1.stop();
+    stepper2.stop();
+  }
+  else if (command.startsWith("m"))
+  {
+    parseMultipleMoveCommands(command);
   }
 }
 
 void parseMultipleMoveCommands(String command)
 {
     /*
-    This function is used to parse the move command of multiple motors and extract the sub-commands
-    Example : m002000m101000m201500 input will be parsed into 3 sub-commands : m002000, m101000, m201500
+    * This function is used to parse the move command of multiple motors and extract the sub-commands
+    * Example : m002000m101000m201500 input will be split into 3 sub-commands : m002000, m101000, m201500
     */
     Serial.println("================================================");
     Serial.println("Parsing input command: " + command);
@@ -48,18 +47,21 @@ void parseMultipleMoveCommands(String command)
             subCommand = command.substring(separatorIndex, nextSeparatorIndex); // Get the sub-command
             separatorIndex = nextSeparatorIndex; // Move to the next 'm'
         }
-
         Serial.println("Sub-command received: " + subCommand);
-
         processCommand(subCommand);
-    
     }
 }
 
 void processCommand(String command) {
-  // Example command format: m002000 (1st joint, clockwise, 2000 steps)
-  if (command.startsWith("m")) {
+  
+  /* 
+  * This is a non-blocking function to process the steppers move command 
+  * Can be used to move multiple motors simultaneously 
+  * Example command format: m002000 (1st joint, clockwise, 2000 steps)
+  * Example command format: m101000 (2nd joint, clockwise, 1000 steps)
+  */
 
+  if (command.startsWith("m")) {
     Serial.println("Processing command: " + command);
     int motorId = command.charAt(1) - '0'; // Get motor ID (0 or 1)
     int direction = command.charAt(2) - '0'; // Get direction (0 = clockwise, 1 = counter-clockwise)
@@ -69,23 +71,54 @@ void processCommand(String command) {
     Serial.println("Direction: " + String(direction));
     Serial.println("Steps: " + String(steps));
 
-    // Adjust steps based on direction
-    //if (direction == 1) {
-    //  steps = -steps; // Reverse steps for counter-clockwise
-    //}
-
     // Move the appropriate motor
     if (motorId == 0) {
       stepper1.moveTo(steps); // Move stepper 1
     } else if (motorId == 1) {
       stepper2.moveTo(steps); // Move stepper 2
     }
+  }
+}
 
+void setup() {
+  Serial.begin(9600); // Start serial communication
+  stepper1.setMaxSpeed(500); // Set maximum speed for stepper 1
+  stepper1.setAcceleration(1000); // Set acceleration for stepper 1
+  stepper2.setMaxSpeed(500); // Set maximum speed for stepper 2
+  stepper2.setAcceleration(1000); // Set acceleration for stepper 2
+}
+
+// Array to store the state of the steppers
+bool stepperIsMoving[2] = {false, false};
+
+void loop() {
+  if (Serial.available() > 0) {
+    String command = Serial.readStringUntil('\n'); // Read command until newline    
+    parseInputCommand(command); // Process the command
+  }
+  else
+  {
     // Move both motors simultaneously
-    if (stepper1.distanceToGo() != 0 || stepper2.distanceToGo() != 0)
+    if (stepper1.distanceToGo() != 0)
     {
       stepper1.run();
+      stepperIsMoving[0] = true;
+    }
+    else if (stepperIsMoving[0] == true)
+    {
+      Serial.println("Stepper 1 is at the destination :" + String(stepper1.currentPosition()));
+      stepperIsMoving[0] = false;
+    }
+    
+    if (stepper2.distanceToGo() != 0)
+    {
       stepper2.run();
+      stepperIsMoving[1] = true;
+    }
+    else if (stepperIsMoving[1] == true)
+    {
+      Serial.println("Stepper 2 is at the destination :" + String(stepper2.currentPosition()));
+      stepperIsMoving[1] = false;
     }
   }
 }
